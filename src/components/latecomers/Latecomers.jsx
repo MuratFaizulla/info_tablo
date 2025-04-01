5031cc327864316e05b2a361f7dfbbca041c50cd
MuratFaizulla <muratf345@gmail.com>
2025-04-01 15:01:42 +0500

    family_four_quarter commit

--- src/components/latecomers/Latecomers.jsx
+++ src/components/latecomers/Latecomers.jsx
@@ -1,47 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels"; // Импорт плагина
 import styles from "./Latecomers.module.css";
 
+// Регистрация плагинов
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartDataLabels
+);
+
 const fetchLatecomers = async () => {
-  const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/latecomers`);
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_API_URL}/family_four_quarter`
+  );
   return data;
 };
 
 const houseNames = {
-  "Қазығұрт": ["7C", "8A", "9N", "10N"],
-  "Атамекен": ["7N", "9M", "11A", "12A"],
-  "Ордабасы": ["7B", "10K", "11K", "12B"],
-  "Хантағы": ["7A", "8L", "10A", "12C"],
-  "Мұзтау": ["7D", "8M", "9C", "10C"],
-  "Тұран": ["8F", "8D", "9A", "9K"],
-  "Алаш": ["7F", "9F", "10L", "12K"],
-  "Отырар": ["7K", "8B", "9B", "10F"],
-  "Оқжетпес": ["7E", "8N", "10E"],
-  "Фараб": ["7L", "8C", "8K", "10M"],
-  "Арыстанды": ["7M", "9D", "9E", "10D"],
-  "Яссы": ["7G", "8E", "9L", "10B"]
+  Қазығұрт: ["7C", "8A", "9N", "10N"],
+  Атамекен: ["7N", "9M", "11A", "12A"],
+  Ордабасы: ["7B", "10K", "11K", "12B"],
+  Хантағы: ["7A", "8L", "10A", "12C"],
+  Мұзтау: ["7D", "8M", "9C", "10C"],
+  Тұран: ["8F", "8D", "9A", "9K"],
+  Алаш: ["7F", "9F", "10L", "12K"],
+  Отырар: ["7K", "8B", "9B", "10F"],
+  Оқжетпес: ["7E", "8N", "10E"],
+  Фараб: ["7L", "8C", "8K", "10M"],
+  Арыстанды: ["7M", "9D", "9E", "10D"],
+  Яссы: ["7G", "8E", "9L", "10B"],
 };
 
 const getHouseName = (className) => {
-  return Object.keys(houseNames).find(house => houseNames[house].includes(className)) || "Неизвестный шаңырақ";
+  return (
+    Object.keys(houseNames).find((house) =>
+      houseNames[house].includes(className)
+    ) || "Неизвестный шаңырақ"
+  );
 };
 
 const Latecomers = () => {
-  const { data: latecomers, isLoading, error } = useQuery("latecomers", fetchLatecomers, {
+  const {
+    data: latecomers,
+    isLoading,
+    error,
+  } = useQuery("latecomers", fetchLatecomers, {
     refetchInterval: 60000,
   });
-
-  const [expandedHouse, setExpandedHouse] = useState(null);
-
-  const toggleHouse = (house) => {
-    setExpandedHouse(expandedHouse === house ? null : house);
-  };
-
-  const formatTime = (dateString) => {
-    const date = new Date(dateString);
-    return date.toLocaleTimeString();
-  };
 
   if (isLoading) {
     return <div className={styles.loading}>Загрузка...</div>;
@@ -56,57 +77,88 @@ const Latecomers = () => {
     HOUSE_NAME: getHouseName(person.CLASS_NAME),
   }));
 
+  // Группировка по шаңыракам
   const groupedByHouse = Object.keys(houseNames).reduce((acc, house) => {
-    acc[house] = [];
+    acc[house] = 0; // инициализация счетчика
     return acc;
   }, {});
 
-  enrichedLatecomers.forEach(person => {
-    groupedByHouse[person.HOUSE_NAME].push(person);
+  // Считаем количество опоздавших по каждому шаңыраку
+  enrichedLatecomers.forEach((person) => {
+    groupedByHouse[person.HOUSE_NAME]++;
   });
+
+  // Сортировка по количеству опоздавших (по убыванию)
+  const sortedGroupedByHouse = Object.entries(groupedByHouse)
+    .sort((a, b) => b[1] - a[1]) // Сортируем по значению (количеству)
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+
+  // Данные для графика
+  const chartData = {
+    labels: Object.keys(sortedGroupedByHouse), // Названия шаңыраков
+    datasets: [
+      {
+        label: "Опоздавшие",
+        data: Object.values(sortedGroupedByHouse), // Количество опоздавших
+        backgroundColor: "#607D8B", // Синий
+        borderColor: "rgba(54, 162, 235, 1)",
+        hoverBackgroundColor: "rgba(54, 162, 235, 1)",
+        hoverBorderColor: "rgba(54, 162, 235, 1)",
+      },
+    ],
+  };
 
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Статистика опозданий по шаңырақам</h2>
-      <div className={styles.housesContainer}>
-        {Object.entries(groupedByHouse).map(([house, students]) => (
-          <div key={house} className={styles.houseCard}>
-            <h3 className={styles.houseTitle} onClick={() => toggleHouse(house)}>
-              {house} <span className={styles.count}>(Опоздавших: {students.length})</span>
-            </h3>
-            {expandedHouse === house && (
-              <div className={styles.tableContainer}>
-                <table className={styles.table}>
-                  <thead>
-                    <tr>
-                      <th>ФИО</th>
-                      <th>Класс</th>
-                      <th>Время входа</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {students.length > 0 ? (
-                      students.map((person, index) => (
-                        <tr key={index}>
-                          <td>{person.FULL_FIO}</td>
-                          <td>{person.CLASS_NAME}</td>
-                          <td>{formatTime(person.FIRST_ENTRY)}</td>
-                        </tr>
-                      ))
-                    ) : (
-                      <tr>
-                        <td colSpan="3" style={{ textAlign: "center" }}>Нет опоздавших</td>
-                      </tr>
-                    )}
-                  </tbody>
-                </table>
-              </div>
-            )}
-          </div>
-        ))}
+      <div className={styles.chartContainer}>
+        <Bar
+          data={chartData}
+          options={{
+            responsive: true,
+            plugins: {
+              title: {
+                display: true,
+                text: "Опоздавшие на 4 четверть",
+              },
+              tooltip: {
+                enabled: false, // Отключаем тултипы
+              },
+              datalabels: {
+                display: true,
+                color: "white", // Цвет текста на столбцах
+                font: {
+                  weight: "bold",
+                  size: 14,
+                },
+                formatter: (value) => `${value} `, // Форматируем данные на столбцах
+              },
+            },
+            scales: {
+              y: {
+                beginAtZero: true,
+                grid: {
+                  display: false, // Убираем сетку по оси Y
+                },
+                ticks: {
+                  display: false, // Убираем цифры на оси Y
+                },
+              },
+              x: {
+                grid: {
+                  display: false, // Убираем сетку по оси X
+                },
+              },
+            },
+          }}
+        />
       </div>
     </div>
   );
 };
+
 
 export default Latecomers;
